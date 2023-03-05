bbd7adcb04867e2b8c2ef7e8cfdd95489e825998
Vinícius Melesque <101480393+vinimelesque@users.noreply.github.com>
2023-03-04 23:39:47 -0300

    commit inicial
    O meu primeiro Commit de um site, buscando aprender mais a mexer no GitHub.

---
+++ script.js
@@ -0,0 +1,12 @@
+// Declaração de variável
+
+num1 = (parseInt(prompt('Digite o primeiro número: ')));
+// parseInt -> transforma uma string em um número inteiro.
+// string -> caractere.
+
+num2 = (parseInt(prompt('Digite o segundo número: ')));
+
+soma = (num1 + num2); // soma dos números.
+
+document.writeln('<h1>' +soma+ '<h1>'); // imprime a soma dos dois números na tela.
+//document.writeln -> Grava uma seqüência de texto, seguido por um caractere de nova linha a um documento.
